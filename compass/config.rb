252602d61273bf3d0ad9c2c8d94bc06558dfed3e
Shia Du <xtd0822@gmail.com>
2026-05-04 22:51:15 -0400

    Deploy core website release (web assets only, large source files excluded)

---
+++ compass/config.rb
@@ -0,0 +1,12 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path 		= "../"
+css_dir 		= "../css"
+sass_dir 		= "sass"
+images_dir 		= "../images"
+javascripts_dir = "../js"
+fonts_dir		= "../fonts"
+
+output_style 	= :compressed
+line_comments 	= false
